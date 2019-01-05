8c0d709bb1fafc91e7810fdfe04f32f0c4f15420
jojoldu <jojoldu@gmail.com>
2019-01-05 12:51:27 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/lecture/fcspringboot/FcSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.jojoldu.lecture.fcspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcSpringBootApplication.class, args);
+    }
+
+}
+
