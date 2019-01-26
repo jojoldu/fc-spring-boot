2f3be8a05b0629b3b5d3d0aaa828aeef5fdce6e7
jojoldu <jojoldu@gmail.com>
2019-01-26 18:01:18 +0900

    [4주차] 4주차 끝

---
+++ src/main/java/com/jojoldu/lecture/fcspringboot/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.jojoldu.lecture.fcspringboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by jojoldu@gmail.com on 2019-01-26
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
